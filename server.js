b47ab09046714118ae469c558422872500198db7
Whitney Seiler <whitney.seiler@gmail.com>
2018-03-30 14:40:40 -0700

    push version for deployment

--- server.js
+++ server.js
@@ -16,43 +16,47 @@ app.get('/rooms/:roomid', (req,res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-// Photo Carousel Redirection
-app.get('/api/rooms/:roomid/carousel', (req, res) =>{
-  res.redirect(`http://18.222.4.195:3001/api/rooms/${req.params.roomid}/carousel`);
-});
-
-// Description Redirection
-app.get('/api/rooms/:roomid/description', (req,res) => {
-  res.redirect(`http://18.222.4.195/api/rooms/${req.params.roomid}/description`);
-});
+// // Photo Carousel Redirection
+// app.get('/api/rooms/:roomid/carousel', (req, res) =>{
+//   res.redirect(`http://18.222.4.195:3001/api/rooms/${req.params.roomid}/carousel`);
+// });
+//
+// // Description Redirection
+// app.get('/api/rooms/:roomid/description', (req,res) => {
+//   res.redirect(`http://18.222.4.195/api/rooms/${req.params.roomid}/description`);
+// });
 
 // Booking Redirection
-app.get('/rooms/:roomid/bookings', (req,res) => {
-  res.redirect(`http://localhost:3003/rooms/${req.params.roomid}/bookings`);
+app.get('/:roomid/bookings', (req,res) => {
+  res.redirect(`http://54.67.32.160:3003/rooms/${req.params.roomid}/bookings`);
+});
+app.get('/api/rooms/:roomid/bookings', (req,res) => {
+  res.redirect(`http://54.215.239.60:3003/api/rooms/${req.params.roomid}/bookings`);
 });
 
-// Review Redirections
-app.get('/rooms/:roomid/reviews', (req,res) => {
-  res.redirect(`http://18.219.35.229/rooms/${req.params.roomid}/reviews`);
-});
-app.get('/rooms/:roomid/ratings', (req,res) => {
-  res.redirect(`http://18.219.35.229/rooms/${req.params.roomid}/ratings`);
-});
-
-// Neighborhood Redirection
-app.get('/rooms/:roomid/neighborhood', (req, res) => {
-  res.redirect(`http://18.188.46.228/rooms/${req.params.roomid}/neighborhood`);
-});
-app.get('/api/neighborhood/:roomid', (req, res) => {
-  res.redirect(`http://18.188.46.228/api/neighborhood/${req.params.roomid}`);
-});
-
-// Similar Listing Redirection
-app.get('/rooms/:roomid/similarListings', (req,res) => {
-  res.redirect(`http://localhost:3007/rooms/${req.params.roomid}/similarListings`);
-});
-app.get('/rooms/images/:imagefile', (req,res) => {
-  res.redirect(`http://localhost:3007/images/${req.params.imagefile}`);
-});
+// // Review Redirections
+// app.get('/rooms/:roomid/reviews', (req,res) => {
+//   res.redirect(`http://18.219.35.229/rooms/${req.params.roomid}/reviews`);
+// });
+//
+// app.get('/api/rooms/:roomid/ratings', (req,res) => {
+//   res.redirect(`http://18.219.35.229/rooms/${req.params.roomid}/ratings`);
+// });
+//
+// // Neighborhood Redirection
+// app.get('/rooms/:roomid/neighborhood', (req, res) => {
+//   res.redirect(`http://18.188.46.228/rooms/${req.params.roomid}/neighborhood`);
+// });
+// app.get('/api/neighborhood/:roomid', (req, res) => {
+//   res.redirect(`http://18.188.46.228/api/neighborhood/${req.params.roomid}`);
+// });
+//
+// // Similar Listing Redirection
+// app.get('/rooms/:roomid/similarListings', (req,res) => {
+//   res.redirect(`http://localhost:3007/rooms/${req.params.roomid}/similarListings`);
+// });
+// app.get('/rooms/images/:imagefile', (req,res) => {
+//   res.redirect(`http://localhost:3007/images/${req.params.imagefile}`);
+// });
 
 app.listen(3000);
